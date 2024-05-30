80fd94e09492232eaedb57bfdb216aab8199b3c7
Nguyenhoangdung335 <dungnhse180163@fpt.edu.vn>
2024-05-30 15:38:19 +0700

    Create prototype for custom error handling

---
+++ Back-end/src/main/java/com/swp391/JewelryProduction/util/errorHandler/ApiError.java
@@ -0,0 +1,19 @@
+package com.swp391.JewelryProduction.util.errorHandler;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ApiError {
+    private HttpStatus status;
+    private String message;
+    private List<String> errors;
+}
