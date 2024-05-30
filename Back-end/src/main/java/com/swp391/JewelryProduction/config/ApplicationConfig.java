da0d34eafab5f97ceeb5372c52815fce113cc9ce
Nguyenhoangdung335 <dungnhse180163@fpt.edu.vn>
2024-05-30 15:28:56 +0700

    Add Config for Security and Application

--- Back-end/src/main/java/com/swp391/JewelryProduction/config/ApplicationConfig.java
+++ Back-end/src/main/java/com/swp391/JewelryProduction/config/ApplicationConfig.java
@@ -1,23 +1,56 @@
 package com.swp391.JewelryProduction.config;
 
-import com.swp391.JewelryProduction.repositories.AccountRepository;
-import lombok.RequiredArgsConstructor;
+import com.swp391.JewelryProduction.security.model.UserRepository;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-@RequiredArgsConstructor
+@AllArgsConstructor
+@ComponentScan(basePackages = "com.swp391.JewelryProduction.security")
 public class ApplicationConfig {
 
-    private final AccountRepository accountRepository;
+    private final UserRepository userRepository;
 
     @Bean
     public UserDetailsService userDetailsService() {
-        return username -> accountRepository
+        return username -> userRepository
                 .findByEmail(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService());
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager (
+            AuthenticationConfiguration config
+    ) throws Exception{
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
     }
 }
--- Back-end/src/main/java/com/swp391/JewelryProduction/config/SecurityConfig.java
+++ Back-end/src/main/java/com/swp391/JewelryProduction/config/SecurityConfig.java
@@ -1,37 +1,47 @@
 package com.swp391.JewelryProduction.config;
 
+import com.swp391.JewelryProduction.security.jwt.JWTAuthenticationFilter;
 import com.swp391.JewelryProduction.util.CustomAuthenticationFailureHandler;
 import com.swp391.JewelryProduction.util.CustomLogoutSuccessHandler;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final JWTAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+    private final PasswordEncoder passwordEncoder;
+
     @Bean
     public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
         UserDetails user1 = User.withUsername("user1")
-                .password(passwordEncoder().encode("12345"))
+                .password(passwordEncoder.encode("12345"))
                 .roles("USER")
                 .build();
         UserDetails user2 = User.withUsername("user2")
-                .password(passwordEncoder().encode("56789"))
+                .password(passwordEncoder.encode("56789"))
                 .roles("USER")
                 .build();
         UserDetails admin = User.withUsername("admin")
-                .password(passwordEncoder().encode("adminPass"))
+                .password(passwordEncoder.encode("adminPass"))
                 .roles("ADMIN")
                 .build();
         return new InMemoryUserDetailsManager(user1, user2, admin);
@@ -42,24 +52,31 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request -> request
-                        .requestMatchers("/admin/**").hasRole("ADMIN")
-                        .requestMatchers("/user/**").hasRole("USER")
-                        .requestMatchers("/anonymous").anonymous()
-                        .requestMatchers("/login", "/api").permitAll()
+//                        .requestMatchers("/admin/**").hasRole("ADMIN")
+//                        .requestMatchers("/user/**").hasRole("USER")
+//                        .requestMatchers("/anonymous").anonymous()
+//                        .requestMatchers("/login", "/api").permitAll()
+//                        .anyRequest().authenticated()
+                        .requestMatchers("/api/auth/**", "/api/registration/**").permitAll()
                         .anyRequest().authenticated()
                 )
-                .formLogin(login -> login
-                        .loginPage("/login.jsp")
-                        .loginProcessingUrl("/perform_login")
-                        .defaultSuccessUrl("/homepage.html", true)
-                        .failureUrl("/login.html?error=true")
-                        .failureHandler(authenticationFailureHandler())
+//                .formLogin(login -> login
+//                        .loginPage("/login.jsp")
+//                        .loginProcessingUrl("/perform_login")
+//                        .defaultSuccessUrl("/homepage.html", true)
+//                        .failureUrl("/login.html?error=true")
+//                        .failureHandler(authenticationFailureHandler())
+//                )
+//                .logout(logout -> logout
+//                        .logoutUrl("/perform_logout")
+//                        .deleteCookies("JSESSIONID")
+//                        .logoutSuccessHandler(logoutSuccessHandler())
+//                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
-                .logout(logout -> logout
-                        .logoutUrl("/perform_logout")
-                        .deleteCookies("JSESSIONID")
-                        .logoutSuccessHandler(logoutSuccessHandler())
-                )
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
         ;
 
         return http.build();
@@ -73,9 +90,9 @@ public class SecurityConfig {
     public LogoutSuccessHandler logoutSuccessHandler() {
         return new CustomLogoutSuccessHandler();
     }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
 }
