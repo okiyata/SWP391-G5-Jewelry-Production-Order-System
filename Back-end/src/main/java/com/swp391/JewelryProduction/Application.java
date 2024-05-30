56ce90f78f56f79a8ae2b013b2c918db053176ff
Nguyenhoangdung335 <dungnhse180163@fpt.edu.vn>
2024-05-30 15:27:01 +0700

    Add Component Scan for Security package

--- Back-end/src/main/java/com/swp391/JewelryProduction/Application.java
+++ Back-end/src/main/java/com/swp391/JewelryProduction/Application.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
@@ -14,6 +15,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 
 @SpringBootApplication
+@ComponentScan({"com.swp391.JewelryProduction.security", "com.swp391.JewelryProduction"})
 public class Application   {
 
 	private static final Logger logger = LoggerFactory.getLogger(Application.class);
