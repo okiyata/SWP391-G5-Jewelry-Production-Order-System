6c9e456326fd59896ee0888425986030c4088ad8
Nguyenhoangdung335 <dungnhse180163@fpt.edu.vn>
2024-05-30 15:37:05 +0700

    Clean unnecessary code

--- Back-end/src/main/java/com/swp391/JewelryProduction/repositories/AccountRepository.java
+++ Back-end/src/main/java/com/swp391/JewelryProduction/repositories/AccountRepository.java
@@ -1,16 +1,12 @@
-package com.swp391.JewleryProduction.repositories;
+package com.swp391.JewelryProduction.repositories;
 
-import com.swp391.JewleryProduction.pojos.Account;
+import com.swp391.JewelryProduction.pojos.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface AccountRepository extends JpaRepository<Account, String> {
-<<<<<<< Updated upstream
-    Account findAccountByEmailAndPassword(String email, String password);
-    boolean findAccountByEmail(String email);
-    Account findByEmail(String email);
-=======
-    Optional<Account> findByEmail(String email);
->>>>>>> Stashed changes
+//    Account findAccountByEmailAndPassword(String email, String password);
+//    boolean findAccountByEmail(String email);
+    Optional<Account> findByEmail (String email);
 }
--- Back-end/src/main/java/com/swp391/JewelryProduction/repositories/UserInfoRepository.java
+++ Back-end/src/main/java/com/swp391/JewelryProduction/repositories/UserInfoRepository.java
@@ -3,5 +3,5 @@ package com.swp391.JewelryProduction.repositories;
 import com.swp391.JewelryProduction.pojos.UserInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> {
+public interface UserInfoRepository extends JpaRepository<UserInfo, String> {
 }
