5cce217f77dae564be642ff98c236e1ea1b75187
Nguyenhoangdung335 <dungnhse180163@fpt.edu.vn>
2024-05-31 17:54:15 +0700

    Create Firebase service layer
    Create service classes FirestoreService and FirebaseStorageService

---
+++ Back-end/src/main/java/com/swp391/JewelryProduction/services/FirebaseStorageService.java
@@ -0,0 +1,29 @@
+package com.swp391.JewelryProduction.services;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Service
+public class FirebaseStorageService {
+    private static final String BUCKET_NAME = "your-bucket-name";
+
+    public String uploadFile(MultipartFile file) throws IOException {
+        Storage storage = StorageOptions.getDefaultInstance().getService();
+        Bucket bucket = storage.getBucket(BUCKET_NAME);
+
+        String fileName = UUID.randomUUID().toString() + "-" + file.getOriginalFilename();
+        Blob blob = bucket.create(fileName, file.getInputStream(), file.getContentType());
+
+        return blob.getMediaLink();
+    }
+
+    public byte[] getFile(String fileName) {
+        Storage storage = StorageOptions.getDefaultInstance().getService();
+        Bucket bucket = storage.getBucket(BUCKET_NAME);
+
+        Blob blob = bucket.get(fileName);
+        return blob.getContent();
+    }
+}
---
+++ Back-end/src/main/java/com/swp391/JewelryProduction/services/FirestoreService.java
@@ -0,0 +1,4 @@
+package com.swp391.JewelryProduction.services;
+
+public class FirestoreService {
+}
