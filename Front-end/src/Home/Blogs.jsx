e34c19e95c36dde82e4636696c911d2be6bf48d4
Khai-Tran24 <163870078+Khai-Tran24@users.noreply.github.com>
2024-06-02 13:42:53 +0700

    Update 6/2/2024

--- Front-end/src/Home/Blogs.jsx
+++ Front-end/src/Home/Blogs.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import BlogMain from "../reusable/BlogMain";
-import becomeTeacherImage from "../assets/images/becomeTeacher.jpg";
+import becomeTeacherImage from "../assets/becomeTeacher.jpg";
 import { Link } from "react-router-dom";
 
 export default function Blogs() {
