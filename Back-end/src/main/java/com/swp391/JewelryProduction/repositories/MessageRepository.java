1e7693c0448ae22eee8a2acfa08c9ef769092ebb
Nguyenhoangdung335 <dungnhse180163@fpt.edu.vn>
2024-06-03 12:31:22 +0700

    Remove Message related model

--- Back-end/src/main/java/com/swp391/JewelryProduction/repositories/MessageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.swp391.JewelryProduction.repositories;
-
-import com.swp391.JewelryProduction.pojos.Message;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface MessageRepository extends JpaRepository<Message, UUID> {
-}
--- Back-end/src/main/java/com/swp391/JewelryProduction/services/message/IMessageService.java
+++
@@ -1,9 +0,0 @@
-package com.swp391.JewelryProduction.services.message;
-
-import com.swp391.JewelryProduction.dto.MessageDTO;
-
-import java.util.List;
-
-public interface IMessageService {
-    List<MessageDTO> findAllMessages();
-}
