f61aa2754d353d0f6e886b40362912cdff869f4c
Nguyenhoangdung335 <dungnhse180163@fpt.edu.vn>
2024-06-03 12:38:06 +0700

    Add Embedded id class for StaffOrderID and ProductSpecificationID

--- Back-end/src/main/java/com/swp391/JewelryProduction/util/embeddedID/ProductSpecificationID.java
+++ Back-end/src/main/java/com/swp391/JewelryProduction/util/embeddedID/ProductSpecificationID.java
@@ -4,12 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.io.Serializable;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class StaffOrderID implements Serializable {
-    private String staffID;
-    private String orderID;
+public class ProductSpecificationID {
+    private String productID;
+    private Integer ValueID;
 }
