58017c3c829b1ce5de243073144b554347c90f88
datvtse180371 <134022724+datvtse180371@users.noreply.github.com>
2024-06-04 11:07:52 +0700

    Add ReportController

--- Back-end/src/main/java/com/swp391/JewelryProduction/controller/ReportController.java
+++ Back-end/src/main/java/com/swp391/JewelryProduction/controller/ReportController.java
@@ -4,44 +4,78 @@ import com.swp391.JewelryProduction.enums.OrderStatus;
 import com.swp391.JewelryProduction.enums.ReportType;
 import com.swp391.JewelryProduction.enums.Role;
 import com.swp391.JewelryProduction.enums.WorkStatus;
+import com.swp391.JewelryProduction.pojos.Notification;
 import com.swp391.JewelryProduction.pojos.Order;
 import com.swp391.JewelryProduction.pojos.Report;
 import com.swp391.JewelryProduction.services.account.AccountService;
-import com.swp391.JewelryProduction.services.report.ReportService;
+import com.swp391.JewelryProduction.services.notification.NotificationService;
+import com.swp391.JewelryProduction.services.order.OrderServiceImpl;
+import com.swp391.JewelryProduction.services.report.ReportServiceImpl;
 import com.swp391.JewelryProduction.util.Response;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 @RestController
-@AllArgsConstructor
-@NoArgsConstructor
+@RequiredArgsConstructor
 public class ReportController {
-    private ReportService reportService;
+    private final OrderServiceImpl orderService;
+    private NotificationService notificationService;
+    private ReportServiceImpl reportService;
     private AccountService accountService;
 
+    @PostMapping
+    public ResponseEntity<Response> postCustomerRequest(@Valid @RequestBody Report report) {
+        report.setType(ReportType.REQUEST);
+        Order order = new Order();
+        order.setStatus(OrderStatus.REQUESTING);
+        Notification notification = new Notification(UUID.randomUUID(), report, order, accountService.findAccountByRole(Role.MANAGER));
+        reportService.saveReport(report);
+        orderService.saveOrder(order);
+        notificationService.saveNotification(notification);
+        return Response.builder()
+                .status(HttpStatus.OK)
+                .message("Request send successfully.")
+                .response("request", report)
+                .buildEntity();
+    }
 
     @PostMapping
-    public void getReport(Report report, Order order) {
-        switch (report.getType()) {
-            case REQUEST:
-                order = new Order();
-                order.setStatus(OrderStatus.REQUESTING);
-                order.getReports().add(report);
-                accountService.findAccountByRole(Role.MANAGER).notify(); //notify the manager with the request attached, notify() needs to be replace for another method
-            case QUOTATION: //quotation has to send to the manager and the customer for approval
-                order.setStatus(OrderStatus.QUO_AWAIT_APPROVAL);
-                accountService.findAccountByRole(Role.MANAGER).notify(); //notify the manager with the quotation attached, notify() needs to be replace for another method
-            case ORDER: //after manager approved the quotation and set to order and the customer purchased the quotation
-                order.setStatus(OrderStatus.IN_DESIGNING);
-                order.getReports().add(report);
-                //notify the designer with the report(type: order) attached
-                accountService.findStaffByRoleAndWorkStatus(Role.DESIGN_STAFF, WorkStatus.FREE).notify(); //report with ReportType.ORDER
-        }
-
+    public ResponseEntity<Response> postStaffQuotation(@Valid @RequestBody Report report) {
+        report.setType(ReportType.QUOTATION);
+        reportService.saveReport(report);
+        orderService.findAllOrders().stream().filter(ord -> {
+            return ord.getId().equals(report.getReportingOrder().getId());
+        }).findFirst().get().getReports().add(report);
+        return Response.builder()
+                .status(HttpStatus.OK)
+                .message("Quotation send successfully.")
+                .response("quotation", report)
+                .buildEntity();
     }
+
+    @PostMapping
+    public ResponseEntity<Response> postCustomerOrder(@Valid @RequestBody Report report) {
+        report.setType(ReportType.ORDER);
+        reportService.saveReport(report);
+        orderService.findAllOrders().stream().filter(ord -> {
+            return ord.getId().equals(report.getReportingOrder().getId());
+        }).findFirst().get().getReports().add(report);
+        return Response.builder()
+                .status(HttpStatus.OK)
+                .message("Quotation send successfully.")
+                .response("quotation", report)
+                .buildEntity();
+    }
+
+
 }
