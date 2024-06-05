654244e38e788d0ebc6ff53612d7fc5b75319ba5
Nguyenhoangdung335 <dungnhse180163@fpt.edu.vn>
2024-06-05 16:14:19 +0700

    Add Cross-Origin-Resource-Sharing config

--- Back-end/src/main/java/com/swp391/JewelryProduction/Application.java
+++ Back-end/src/main/java/com/swp391/JewelryProduction/Application.java
@@ -4,15 +4,19 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.swp391.JewelryProduction.services.crawl.CrawlDataService;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -30,6 +34,16 @@ public class Application   {
 //
 //		CrawlDataService crawlDataService = context.getBean(CrawlDataService.class);
 //		crawlDataService.crawData();
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(@NotNull CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+			}
+		};
 	}
 
 
