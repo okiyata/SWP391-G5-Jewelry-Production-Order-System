3d5b13a2e86206eebb6fc1a0a3bb7a31aaf5fb7c
Nguyenhoangdung335 <dungnhse180163@fpt.edu.vn>
2024-06-05 14:33:24 +0700

    Configurate for deployment

--- Back-end/src/main/java/com/swp391/JewelryProduction/services/PaypalService.java
+++ Back-end/src/main/java/com/swp391/JewelryProduction/services/PaypalService.java
@@ -14,60 +14,60 @@ import java.util.Locale;
 @RequiredArgsConstructor
 public class PaypalService {
 
-    private APIContext apiContext;
-
-    public Payment makePayment(
-            Double total,
-            String currency,
-            String method,
-            String intent,
-            String description,
-            String cancelURL,
-            String successURL
-    ) throws PayPalRESTException {
-
-        Amount amount = new Amount();
-        amount.setCurrency(currency);
-        amount.setTotal(
-                String.format(
-                        Locale.forLanguageTag(currency),
-                        "%.2f", total
-                )
-        );
-
-        Transaction transaction = new Transaction();
-        transaction.setDescription(description);
-        transaction.setAmount(amount);
-
-        List<Transaction> transactions = new ArrayList<>();
-        transactions.add(transaction);
-
-        Payer payer = new Payer();
-        payer.setPaymentMethod(method);
-
-        RedirectUrls redirectUrls = new RedirectUrls();
-        redirectUrls.setCancelUrl(cancelURL);
-        redirectUrls.setReturnUrl(successURL);
-
-        Payment payment = new Payment();
-        payment.setIntent(intent);
-        payment.setPayer(payer);
-        payment.setTransactions(transactions);
-        payment.setRedirectUrls(redirectUrls);
-
-        return payment.create(apiContext);
-    }
-
-    public Payment executePayment (
-            String paymentID,
-            String payerID
-    ) throws PayPalRESTException {
-        Payment payment = new Payment();
-        payment.setId(paymentID);
-
-        PaymentExecution execution = new PaymentExecution();
-        execution.setPayerId(payerID);
-
-        return payment.execute(apiContext, execution);
-    }
+//    private APIContext apiContext;
+//
+//    public Payment makePayment(
+//            Double total,
+//            String currency,
+//            String method,
+//            String intent,
+//            String description,
+//            String cancelURL,
+//            String successURL
+//    ) throws PayPalRESTException {
+//
+//        Amount amount = new Amount();
+//        amount.setCurrency(currency);
+//        amount.setTotal(
+//                String.format(
+//                        Locale.forLanguageTag(currency),
+//                        "%.2f", total
+//                )
+//        );
+//
+//        Transaction transaction = new Transaction();
+//        transaction.setDescription(description);
+//        transaction.setAmount(amount);
+//
+//        List<Transaction> transactions = new ArrayList<>();
+//        transactions.add(transaction);
+//
+//        Payer payer = new Payer();
+//        payer.setPaymentMethod(method);
+//
+//        RedirectUrls redirectUrls = new RedirectUrls();
+//        redirectUrls.setCancelUrl(cancelURL);
+//        redirectUrls.setReturnUrl(successURL);
+//
+//        Payment payment = new Payment();
+//        payment.setIntent(intent);
+//        payment.setPayer(payer);
+//        payment.setTransactions(transactions);
+//        payment.setRedirectUrls(redirectUrls);
+//
+//        return payment.create(apiContext);
+//    }
+//
+//    public Payment executePayment (
+//            String paymentID,
+//            String payerID
+//    ) throws PayPalRESTException {
+//        Payment payment = new Payment();
+//        payment.setId(paymentID);
+//
+//        PaymentExecution execution = new PaymentExecution();
+//        execution.setPayerId(payerID);
+//
+//        return payment.execute(apiContext, execution);
+//    }
 }
