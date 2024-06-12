c8a199442f21e114cb903e896dd543dc77839808
Nguyenhoangdung335 <dungnhse180163@fpt.edu.vn>
2024-06-12 10:54:12 +0700

    Refactor ObjectNotFoundException

--- Back-end/src/main/java/com/swp391/JewelryProduction/util/exceptions/ObjectNotFoundException.java
+++ Back-end/src/main/java/com/swp391/JewelryProduction/util/exceptions/ObjectNotFoundException.java
@@ -1,10 +1,13 @@
 package com.swp391.JewelryProduction.util.exceptions;
 
-public class ObjectNotFoundException extends Exception{
+public class ObjectNotFoundException extends RuntimeException{
     public ObjectNotFoundException(){
         super("User not found");
     }
     public ObjectNotFoundException(String message){
         super(message);
+    }
+    public ObjectNotFoundException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
