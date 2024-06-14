0b4d41d3d6b87b09f196d95f48bf2d334a8a6316
Nguyenhoangdung335 <dungnhse180163@fpt.edu.vn>
2024-06-14 12:21:41 +0700

    Add saveOrder method in OrderService

--- Back-end/src/main/java/com/swp391/JewelryProduction/services/order/OrderService.java
+++ Back-end/src/main/java/com/swp391/JewelryProduction/services/order/OrderService.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public interface OrderService {
     List<Order> findAllOrders();
-    void saveOrder(Order order);
     Order findOrderById(String id);
+    Order saveOrder(Order order);
+    Order createNewOrder();
 }
