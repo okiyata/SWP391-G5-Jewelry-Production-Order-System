85a1b43be04fd15706f9eb2dfb4901878a60cf21
Nguyenhoangdung335 <dungnhse180163@fpt.edu.vn>
2024-06-16 12:28:46 +0700

    Add @okiyata User logic for chat app

---
+++ Back-end/src/main/java/com/swp391/JewelryProduction/websocket/user/UserController.java
@@ -0,0 +1,62 @@
+package com.swp391.JewelryProduction.websocket.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+    private final SimpMessagingTemplate messagingTemplate;
+
+    @GetMapping("/user/check/{userId}")
+    public ResponseEntity<User> checkUser(@PathVariable String userId) throws Exception {
+        try {
+            User existingUser = userService.findUserById(userId);
+            if (existingUser != null) {
+                messagingTemplate.convertAndSend("/topic/public", existingUser);
+                return ResponseEntity.ok(existingUser);
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @MessageMapping("/user.disconnectUser")
+    @SendTo("/topic/public")
+    public User disconnectUser(@Payload User user) {
+        userService.disconnect(user);
+        messagingTemplate.convertAndSend("/topic/public", user);
+        return user;
+    }
+
+    @GetMapping("/users/{role}")
+    public ResponseEntity<List<User>> getUsersByRole(@PathVariable String role) {
+        try {
+            List<User> users = userService.findUsersByRole(role);
+            return ResponseEntity.ok(users);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @GetMapping("/{nickname}/role")
+    public ResponseEntity<String> getCurrentRole(@PathVariable String nickname) {
+        String role = userService.getCurrentRole(nickname);
+        return ResponseEntity.ok(role);
+    }
+
+}
