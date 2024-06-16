00d01f873b3ec1fe95ac514b693f6d3127e9085f
Nguyenhoangdung335 <dungnhse180163@fpt.edu.vn>
2024-06-16 13:04:49 +0700

    Update Order Service layer

--- Back-end/src/main/java/com/swp391/JewelryProduction/services/order/OrderService.java
+++ Back-end/src/main/java/com/swp391/JewelryProduction/services/order/OrderService.java
@@ -4,11 +4,11 @@ import com.swp391.JewelryProduction.dto.OrderDTO;
 import com.swp391.JewelryProduction.pojos.Order;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface OrderService {
     List<Order> findAllOrders();
     Order saveNewOrder(String accountId);
     Order findOrderById(String id);
-
     Order updateOrder(Order order);
 }
--- Back-end/src/main/java/com/swp391/JewelryProduction/services/order/OrderServiceImpl.java
+++ Back-end/src/main/java/com/swp391/JewelryProduction/services/order/OrderServiceImpl.java
@@ -1,21 +1,15 @@
 package com.swp391.JewelryProduction.services.order;
 
-import com.swp391.JewelryProduction.dto.OrderDTO;
-import com.swp391.JewelryProduction.enums.OrderEvent;
 import com.swp391.JewelryProduction.enums.OrderStatus;
 import com.swp391.JewelryProduction.pojos.Account;
 import com.swp391.JewelryProduction.pojos.Order;
 import com.swp391.JewelryProduction.repositories.OrderRepository;
 import com.swp391.JewelryProduction.services.account.AccountService;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.statemachine.config.StateMachineFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -23,7 +17,6 @@ public class OrderServiceImpl implements OrderService {
     private final OrderRepository orderRepository;
     private final AccountService accountService;
     private final ModelMapper modelMapper;
-    private final StateMachineFactory<OrderStatus, OrderEvent> stateMachineFactory;
 
     @Override
     public List<Order> findAllOrders() {
@@ -36,7 +29,9 @@ public class OrderServiceImpl implements OrderService {
                 .status(OrderStatus.REQUESTING)
                 .owner(modelMapper.map(accountService.findAccountById(accountId), Account.class))
                 .build();
-        return orderRepository.save(order);
+        order = orderRepository.save(order);
+
+        return order;
     }
 
     @Override
@@ -48,5 +43,4 @@ public class OrderServiceImpl implements OrderService {
     public Order updateOrder(Order order) {
         return orderRepository.save(order);
     }
-
 }
