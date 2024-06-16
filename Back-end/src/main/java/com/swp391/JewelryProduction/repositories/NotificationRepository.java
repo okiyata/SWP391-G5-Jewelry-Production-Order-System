260f0f9ffb4974ae6e4930b293faa6ae89a4bf2b
Nguyenhoangdung335 <dungnhse180163@fpt.edu.vn>
2024-06-16 11:54:35 +0700

    Change Primary key data type

--- Back-end/src/main/java/com/swp391/JewelryProduction/repositories/NotificationRepository.java
+++ Back-end/src/main/java/com/swp391/JewelryProduction/repositories/NotificationRepository.java
@@ -1,14 +1,15 @@
 package com.swp391.JewelryProduction.repositories;
 
+import com.swp391.JewelryProduction.pojos.Account;
 import com.swp391.JewelryProduction.pojos.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
-public interface NotificationRepository extends JpaRepository<Notification, String> {
-    List<Notification> findAllByReceiver_Id(String id);
-    Optional<Notification> findById(UUID id);
-    void deleteById(UUID id);
+public interface NotificationRepository extends JpaRepository<Notification, UUID> {
+    List<Notification> findAllByReceiver_Id(String receiverId);
+    List<Notification> findAllByReceiverAndDeliveredFalse (Account receiver);
+    List<Notification> findAllByReceiver (Account receiver);
+    void deleteAllByReceiver (Account receiver);
 }
